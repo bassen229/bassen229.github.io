57ca053ef18ff2438b428e18a067a60640c44e34
Sebastian H <133505443+bassen229@users.noreply.github.com>
2023-05-13 15:46:52 +0200

    Create fixture table

---
+++ get_table.py
@@ -0,0 +1,23 @@
+import requests
+import numpy as np
+import pandas as pd
+
+url = 'https://fantasy.premierleague.com/api/bootstrap-static/'
+r = requests.get(url)
+json  = r.json()
+df = pd.DataFrame(json['teams'])
+id_dict = {}
+for it in df[['name','id']].values:
+    id_dict[it[1]] = it[0]
+
+url = 'https://fantasy.premierleague.com/api/fixtures/'
+r = requests.get(url)
+json = r.json()
+df = pd.DataFrame(json)
+
+df['team_a'] = df['team_a'].map(id_dict)
+df['team_h'] = df['team_h'].map(id_dict)
+df['kickoff_time'] = pd.to_datetime(df['kickoff_time']).dt.strftime("%Y-%m-%d %H:%M:%S")
+
+df = df[['kickoff_time','team_a','team_a_score','team_h','team_h_score']]
+df.to_json("C:\\Users\\sebas\\OneDrive\\premierleaguepage\\bassen229.github.io\\fixtures.json")
